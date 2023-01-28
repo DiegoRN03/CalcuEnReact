bf95477b1df5ceff47b38d0cde64f5d08c83c231
DiegoRN <101913015+DiegoRN03@users.noreply.github.com>
2023-01-27 22:12:27 -0600

    Solo le añadí comentarios detallados
    👍

--- src/components/myComponentCalc.js
+++ src/components/myComponentCalc.js
@@ -1,91 +1,98 @@
 import { Component } from "react";
 import React from "react";
+//Se crea un componente de tipo funcional llamado MyCalculator que recibe como propiedad "props"
 const MyCalculator = (props) => {
+  // Se utiliza la función "useState" de React para inicializar el estado del componente
+  // El estado incluye la variable "calc" que se inicializa con un objeto vacío con las propiedades:
+  // current: "0" -> almacena el número actual ingresado en la calculadora
+  // total: "0" -> almacena el resultado de las operaciones realizadas
+  // esPrimero: true -> indica si es el primer número ingresado
+  // preOp: "" -> almacena el operador seleccionado
+  const [calc, setCalc] = React.useState({
+    current: "0",
+    total: "0",
+    esPrimero: true,
+    preOp: "",
+  });
+  //Función que maneja los números ingresados en la calculadora, recibe como parámetro "number"
+  function handleNumber(number) {
+    //Se inicializa una variable "nuevoNum" con el valor de "number"
+    let nuevoNum = number;
+    //Se verifica si "esPrimero" es falso, si es así se concatena el nuevo número al número actual
+    if (!calc.esPrimero) {
+      nuevoNum = calc.current + number;
+    }
 
-     const [calc, setCalc]  = React.useState({
+    //Se actualiza el estado, asignando el valor de "nuevoNum" a "current"
+    //Además, se mantiene el valor de "total", se cambia "esPrimero" a falso y se mantiene el valor de "preOp"
+    setCalc({
+      current: nuevoNum,
+      total: calc.total,
+      esPrimero: false,
+      preOp: calc.preOp,
+    });
+  }
+
+  //Función que maneja los operadores seleccionados en la calculadora, recibe como parámetro "number"
+
+  function handleOperator(number) {
+    // Realiza una operacion con la funcion Do_Calcular()
+    // la cual retorna el resultado de la operacion
+    const total = Do_Calcular();
+
+    //Actualiza el estado de la calculadora
+    setCalc({
+      //Asigna el valor de "total" como string a "current" y "total"
+      current: total.toString(),
+      total: total.toString(),
+      //Asigna el valor true a "esPrimero" para indicar que se ha terminado la operacion anterior
+      esPrimero: true,
+      //Asigna el valor de "number" (operador) a "preOp"
+      //para almacenar el ultimo operador utilizado
+      preOp: number,
+    });
+  }
+
+  function Do_Calcular() {
+    let total = parseInt(calc.total);
+
+    switch (calc.preOp) {
+      case "+":
+        total += parseInt(calc.current);
+        break;
+      case "*":
+        total *= parseInt(calc.current);
+        break;
+      case "/":
+        total /= parseInt(calc.current);
+        break;
+      case "-":
+        total -= parseInt(calc.current);
+        break;
+      default:
+        total = parseInt(calc.current);
+    }
+    // se devuelve el resultado
+    return total;
+  }
+
+  // Esta función se encarga de renderizar el valor actual en pantalla.
+  // Se toma el valor "current" del objeto "calc" y se devuelve como una cadena.
+  function renderDisplay(number) {
+    return calc.current;
+  }
+  // Esta función se encarga de reiniciar el estado de la calculadora a sus valores predeterminados.
+  // Se asigna a "current" y "total" el valor "0", a "esPrimero" el valor true y a "preOp" el valor vacío.
+  function handleClear() {
+    setCalc({
       current: "0",
       total: "0",
       esPrimero: true,
-      preOp: ""
-     })
-
-     function handleNumber(number) {
-      let nuevoNum = number;
-      if (!calc.esPrimero){
-        nuevoNum = calc.current + number
-      }
-      
-
-      setCalc({current: nuevoNum, total: calc.total, esPrimero: false, preOp: calc.preOp})
-
-     }
-
-    
-
-
-//después:
-function handleOperator(number){
-  if (number === "=") {
-    handleEquals();
-  } else {
-    const total = Do_Calcular();
-    setCalc({current: total.toString(),total:total.toString(), esPrimero: true, preOp: number});
+      preOp: "",
+    });
   }
-}
-
-
-  
-     
-      function Do_Calcular(){
-       
-         let total = parseInt(calc.total)
-     
-        switch (calc.preOp) {
-          case "+":
-            total += parseInt(calc.current);
-            break;
-          case "*":
-            total *= parseInt(calc.current);
-            break;
-          case "/":
-            total /= parseInt(calc.current);
-            break;
-          case "-":
-            total -= parseInt(calc.current);
-            break;
-        }
-        return total;
-      }
-
-   
-
-      //Opcional
-     function renderDisplay (number){
-      
-      return calc.current
-     }
-     
-     function handleClear() {
-      setCalc({
-        current: "0",
-        total: "0",
-        esPrimero: true,
-        preOp: "",
-      });
-     }
-
-     function handleEquals () {
-      let total = Do_Calcular()
-         setCalc({
-           current: total.toString(),
-           total: total.toString(),
-           esPrimero: true,
-           preOp: "=",
-         });
-     }
 
   return (
-
     <div className="calculator">
       <div className="display">{renderDisplay()}</div>
       <CalcButton number="7" onClick={handleNumber} />
@@ -112,15 +119,20 @@ function handleOperator(number){
 }
 
 const CalcButton =  (props) => {
-  return ( 
-    <button className={props.className} 
-    //Funcion callback cuando se da click ejecutamos el metodo onClick que nos regresa un number
-    onClick={ () => props.onClick(props.number)}>
-    {props.number} </button>
-  )
+  return (
+    // Se crea un boton con una clase que se recibe como propiedad
+    <button
+      className={props.className}
+      //Funcion callback cuando se da click ejecutamos el metodo onClick que nos regresa un number
+      // el numero se recibe como propiedad number
+      onClick={() => props.onClick(props.number)}
+    >
+      {props.number}
+    </button>
+  );
 }
 
 
-
-
+//Esta funcion crea un boton que se puede usar para mostrar un numero en la calculadora
+//El boton tiene una clase que se recibe como propieda y tiene un evento OnClick que se activa cuando se presiona el botón. El evento OnClick ejecuta una funcion que se recibe como propiedad y le pasa el numero del boton como argumento. El numero se muestra en el boton
 export default MyCalculator;
